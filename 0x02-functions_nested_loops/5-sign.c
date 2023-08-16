3a83954830594e22d8312c26a65819a557125af2
Khalid Benjelloun <khalidmailing@gmail.com>
2023-08-16 11:05:39 +0100

    edit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - Print the sign of an int
+ * @n: an integer
+ * Return: -1, 0, 1
+ */
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return(-1);
+	}
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	_putchar('0');
+	return (0);
+}
--- 0x02-functions_nested_loops/test.c
+++ 0x02-functions_nested_loops/test.c
@@ -1,12 +1,9 @@
 #include "main.h"
-
+#include <stdlib.h>
 
 int main(int ac, char **av)
 {
 	if (ac == 2)
-	{
-		if (_islower(av[1][0]))
-			_putchar('L');
-	}
-	return 0;
+		print_sign(atoi(av[1]));
+	return (0);
 }
