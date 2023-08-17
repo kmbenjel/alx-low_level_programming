d0ecc0e3a75f762b5aacfbba02fe9b8c821c0673
Khalid Benjelloun <khalidmailing@gmail.com>
2023-08-17 03:17:56 +0100

    edit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Fib4
+ * Description: Write a program that finds and prints the first
+ * 98 Fibonacci numbers, starting with 1 and 2, followed by a new line
+ * Return: 0
+ */
+
+
+
+int main(void)
+{
+	unsigned int long i;
+	unsigned int long c;
+	unsigned int long next;
+	unsigned int long temp;
+
+	i = 0;
+	next = 1;
+	c = 0;
+	temp = 0;
+
+	while (c < 97)
+	{
+		if ((i + next) / 100000)
+			printf("%ld", (i + next) / 100000);
+		printf("%ld, ", (i + next) % 100000);
+		temp = i;
+		i = next;
+		next = temp + next;
+		c++;
+	}
+	if ((i + next) / 100000)
+		printf("%ld", (i + next) / 100000);
+	printf("%ld\n", (i + next) % 100000);
+	return (0);
+}
