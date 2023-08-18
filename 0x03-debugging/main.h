238c37a1e328a5fac415f9606a20cbe3f6fb8ae2
Khalid Benjelloun <khalidmailing@gmail.com>
2023-08-18 07:45:05 +0100

    edit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,4 +6,6 @@
 void _putchar(int c);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
 #endif /* MAIN_H */
