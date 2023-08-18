ef6e6d9aeaa470893cc9f93660bceff8d6957de1
Khalid Benjelloun <khalidmailing@gmail.com>
2023-08-18 09:20:15 +0100

    edit

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
 * main - takes a date and prints how many days are left in the year, taking
@@ -7,21 +8,23 @@
 * Return: 0
 */
 
-int main(void)
+int main(int ac, char **av)
 {
+	if (ac == 4) {
 	int month;
 	int day;
 	int year;
 
-	month = 2;
-	day = 29;
-	year = 2000;
+	month = atoi(av[1]);
+	day = atoi(av[2]);
+	year = atoi(av[3]);
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 
 	day = convert_day(month, day);
 
 	print_remaining_days(month, day, year);
+	}
 
 	return (0);
 }
