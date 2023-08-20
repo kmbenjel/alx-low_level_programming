feb21c0393640d25e844d0e1fae3856e8637af8f
Khalid Benjelloun <khalidmailing@gmail.com>
2023-08-20 09:07:15 +0100

    edit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,7 +12,8 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		print_number(-n / 10);
+		if (n / 10)
+			print_number(-n / 10);
 		_putchar(-(n % 10) + '0');
 	}
 	else if (n >= 10)
