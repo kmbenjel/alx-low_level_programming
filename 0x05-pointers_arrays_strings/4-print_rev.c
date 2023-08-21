804572c71104dda936ea434de7726932340df6f2
Khalid Benjelloun <khalidmailing@gmail.com>
2023-08-21 07:02:48 +0100

    edit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,8 +1,19 @@
 #include "main.h"
 
 /**
- *
- * Description:
- *
- * Return:
+ * print_rev - print reverse string
+ * Description: Prints a reversed string followed by a new line
+ * @s: a string
  */
+
+
+void print_rev(char *s)
+{
+	int i = _strlen(s);
+
+	while (s[--i])
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
