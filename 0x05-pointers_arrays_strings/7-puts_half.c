56f2ad0f04b38fd2c5f930e53905daafc7f8d447
Khalid Benjelloun <khalidmailing@gmail.com>
2023-08-21 08:16:32 +0100

    edit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,8 +1,23 @@
 #include "main.h"
+#include "2-strlen.c"
 
 /**
- *
- * Description:
- *
- * Return:
+ * puts_half - print the second half of a string
+ * Description: Write a function that prints half of
+ * a string, followed by a new line.
+ * @str: a string
  */
+
+
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (i >= _strlen(str) / 2)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
