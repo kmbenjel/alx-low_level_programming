6b237430a520d26d135b133a2f6303d3e9a859dd
Khalid Benjelloun <khalidmailing@gmail.com>
2023-08-21 07:52:34 +0100

    edit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,8 +1,24 @@
 #include "main.h"
+#include "1-swap.c"
+#include "2-strlen.c"
 
 /**
- *
- * Description:
- *
- * Return:
+ * rev_string - reverses a string
+ * Description: reverses a string
+ * @s: a string
  */
+
+
+void rev_string(char *s)
+{
+	int i = 0;
+	char temp;
+
+	while (s[i] && i < _strlen(s) / 2)
+	{
+		temp = s[i];
+		s[i] = s[_strlen(s) - i - 1];
+		s[_strlen(s) - i - 1] = temp;
+		i++;
+	}
+}
