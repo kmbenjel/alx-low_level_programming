6c75dbd9ff6ef1710f4e44a3e131e90dd88bf119
Khalid Benjelloun <khalidmailing@gmail.com>
2023-08-25 01:16:06 +0100

    edit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,21 @@
 #include "main.h"
+
+/**
+ * _strncpy - Copy n chars
+ * Description: The strncpy() function is similar,
+ * except that at most n bytes of src are copied.
+ * Return: char *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
