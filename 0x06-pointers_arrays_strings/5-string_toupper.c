a4c2c1e4527b43150bd8a25a687717e9e6a593e8
Khalid Benjelloun <khalidmailing@gmail.com>
2023-08-25 02:42:43 +0100

    edit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,21 @@
 #include "main.h"
+
+/**
+ * string_toupper - String to Uppercase
+ * Description: All lowcases to uppercase
+ * @s: char *
+ * Return: char *s
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		i++;
+	}
+	return (s);
+}
