fda6892293958854630dae4ce96c609ebac98e0e
Khalid Benjelloun <khalidmailing@gmail.com>
2023-08-25 03:21:17 +0100

    edit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - Leet
+ * Discription: Write a function that
+ * encodes a string into 1337
+ * @s: a string
+ * Return: s
+ */
+
+char *leet(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == 'a' || s[i] == 'A')
+			s[i] = '4';
+		if (s[i] == 'e' || s[i] == 'E')
+			s[i] = '3';
+		if (s[i] == 'o' || s[i] == 'O')
+			s[i] = '0';
+		if (s[i] == 't' || s[i] == 'T')
+			s[i] = '7';
+		if (s[i] == 'l' || s[i] == 'L')
+			s[i] = '1';
+		i++;
+	}
+	return (s);
+}
