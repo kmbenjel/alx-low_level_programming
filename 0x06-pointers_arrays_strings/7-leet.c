58e2e07728caeccbc45bd6d02a29ae7ba96d49c2
Khalid Benjelloun <khalidmailing@gmail.com>
2023-08-25 03:41:06 +0100

    edit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,20 +10,20 @@
 
 char *leet(char *s)
 {
-	int i = 0;
+	int i = 0, j = 0;
+
+	char aeotl[6] = "aeotl";
+	char leet[6] = "43071";
 
 	while (s[i])
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		j = 0;
+		while (aeotl[j])
+		{
+			if (s[i] == aeotl[j] || s[i] == aeotl[j] - 32)
+				s[i] = leet[j];
+			j++;
+		}
 		i++;
 	}
 	return (s);
