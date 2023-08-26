f46470fcf624d0d5ea9b4f6c2d0817124d807f46
Khalid Benjelloun <khalidmailing@gmail.com>
2023-08-26 17:12:04 +0100

    edit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,7 @@
 #include "main.h"
 
-/**
- * space_or_sign - is space or sign
- * Description: Whether the char is a space or not
- * @c: a char
- * Return: 0 or 1
- */
 
-/*
-static char space_or_sign(char c)
-{
-	char *str = "\t\n\v\f\r+- ";
-	int i = 0;
 
-	while (str[i])
-	{
-		if (c == str[i])
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-*/
 
 /**
  * is_digit - Is digit
