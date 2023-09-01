071403dccc45be1a95633b356834c3076d0b2cd7
Khalid Benjelloun <khalidmailing@gmail.com>
2023-09-02 00:08:45 +0100

    edit

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,129 @@
+#include "main.h"
+
+/**
+ * main - Multiply Args
+ * Description - Above
+ * @ac: int
+ * @av: char**
+ * Return: 0
+ */
+
+int main(int ac, char **av)
+{
+	int sum = 0, coins;
+	char *error = "Error\n";
+
+	if (ac != 2)
+	{
+		_puts(error);
+		return (1);
+	}
+
+	coins = _atoi(av[1]);
+	if (coins < 0)
+	{
+		_puts("0\n");
+		return (0);
+	}
+
+	sum += coins / 25;
+	coins = coins % 25;
+	sum += coins / 10;
+	coins = coins % 10;
+	sum += coins / 5;
+	coins = coins % 5;
+	sum += coins / 2;
+	coins = coins % 2;
+	sum += coins;
+	print_number(sum);
+	_putchar('\n');
+	return (0);
+}
+
+/**
+ * _puts - Put string
+ * Description: Above
+ * @s: the string to put
+ */
+
+void _puts(char *s)
+{
+	int i = -1;
+
+	while (s[++i])
+		_putchar(s[i]);
+}
+
+/**
+ * is_digit - Is digit
+ * Description: Is digit
+ * @c: a char
+ * Return: 0 or 1
+ */
+
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+/**
+ * _atoi - ASCII to  Int
+ * Description: Reproduce the behavior of atoi
+ * function of the strandard C library
+ * @s: char *
+ * Return: an int
+ */
+
+int _atoi(char *s)
+{
+	unsigned int result = 0;
+	int sign = 1, i = 0;
+
+	if (!s)
+		return (0);
+
+	while (s[i] && !is_digit(s[i]))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+
+	while (s[i] == '0')
+		i++;
+
+	while (is_digit(s[i]))
+	{
+		result *= 10;
+		result += s[i] - 48;
+		i++;
+	}
+	return (result * sign);
+}
+
+/**
+ * print_number - Print an Int
+ * Description: Write a function that prints an integer
+ * @n: an integer
+ */
+
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		if (n / 10)
+			print_number(-n / 10);
+		_putchar(-(n % 10) + '0');
+	}
+	else if (n >= 10)
+	{
+		print_number(n / 10);
+		_putchar((n % 10) + '0');
+	}
+	else
+		_putchar(n + '0');
+}
