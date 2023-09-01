9892fdb7c4a26d23848c1f9643ab54afb6be5a7a
Khalid Benjelloun <khalidmailing@gmail.com>
2023-09-01 19:29:25 +0100

    edit

--- 0x0A-argc_argv/101-print_number.c
+++ 0x0A-argc_argv/101-print_number.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * main - Argument Count
- * Description - Above
- * @ac: int
- * @av: char**
- * Return: 0
- */
-
-int main(int ac, char **av)
-{
-	if (!ac || !av)
-		return (0);
-
-	print_number(ac - 1);
-	_putchar('\n');
-	return (0);
-}
-
-/**
  * print_number - Print an Int
  * Description: Write a function that prints an integer
  * @n: an integer
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,5 +4,7 @@
 #include <stdio.h>
 
 void _putchar(int);
+void print_number(int);
+
 
 #endif
