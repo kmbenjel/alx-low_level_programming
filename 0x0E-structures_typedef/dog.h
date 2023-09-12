1d50efd3dee6fa2e4a0a753fbe7058ccef649e9d
Khalid Benjelloun <khalidmailing@gmail.com>
2023-09-12 04:26:25 +0100

    edit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+# define DOG_H
+
+/**
+ * struct dog - Dog
+ * @name: char
+ * @age: float
+ * @owner: char
+ * Description: Dog properties
+ */
+
+typedef struct dog
+{
+	char	*name;
+	float	age;
+	char	*owner;
+}
+
+#endif
