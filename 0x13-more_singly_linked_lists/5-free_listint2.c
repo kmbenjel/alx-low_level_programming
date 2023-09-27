bad31f29c62dbe231d360a25b14002d8e9761a05
Khalid Benjelloun <khalidmailing@gmail.com>
2023-09-27 19:38:56 +0100

    edit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,6 @@
-#include "lists.h"
 #include "4-free_listint.c"
+#include "lists.h"
+
 /**
  * free_listint2 - free a list
  * Description: Write a function that frees a listint_t list
@@ -8,6 +9,8 @@
 
 void	free_listint2(listint_t **head)
 {
+	if (!head || !*head)
+		return;
 	free_listint(*head);
 	*head = NULL;
 }
