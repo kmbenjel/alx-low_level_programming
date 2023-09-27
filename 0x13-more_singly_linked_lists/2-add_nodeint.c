09280b0c941d98c4d9059f02e16c0e236354e560
Khalid Benjelloun <khalidmailing@gmail.com>
2023-09-27 21:38:46 +0100

    edit

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -6,20 +6,20 @@
  * node at the beginning of a listint_t list.
  * @head: listint_t **
  * @n: const int
- * Return: listint_t
+ * Return: listint_t *
  */
 
 listint_t	*add_nodeint(listint_t **head, const int n)
 {
 	listint_t	*new;
 
-	if (!head || !*head)
+	if (!head)
 		return (NULL);
 	new = (listint_t *)malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
-	new->next = *head;
 	new->n = n;
+	new->next = *head;
 	*head = new;
 	return (*head);
 }
