9fd67d4b069480a8eace193e794c2dd716e6d228
Khalid Benjelloun <khalidmailing@gmail.com>
2024-06-07 03:38:18 +0100

    edit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,16 +9,18 @@
 def island_perimeter(grid):
     """Calculate the perimeter of the island"""
     perimeter = 0
+    width = len(grid[0])
+    height = len(grid)
     for y in range(len(grid)):
         for x in range(len(grid[0])):
             if grid[y][x] == 1:
                 perimeter += 4
-            if grid[y][x - 1] == 1:
-                perimeter -= 1
-            if grid[y][x + 1] == 1:
-                perimeter -= 1
-            if grid[y - 1][x] == 1:
-                perimeter -= 1
-            if grid[y + 1][x] == 1:
-                perimeter -= 1
+                if x > 0 and grid[y][x - 1] == 1:
+                    perimeter -= 1
+                if x < width - 1 and grid[y][x + 1] == 1:
+                    perimeter -= 1
+                if y > 0 and grid[y - 1][x] == 1:
+                    perimeter -= 1
+                if y < height - 1 and grid[y + 1][x] == 1:
+                    perimeter -= 1
     return perimeter
--- 0x1C-makefiles/5-main.py
+++ 0x1C-makefiles/5-main.py
@@ -5,16 +5,6 @@
 island_perimeter = __import__('5-island_perimeter').island_perimeter
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     grid = [[0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0],
             [0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0]]
-=======
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
->>>>>>> ff4aa8b19ad1e200b148f1547a3deca48476907d
     print(island_perimeter(grid))
