3a8094434753431428ad2a8ce9580e805e2a4037
Khalid Benjelloun <khalidmailing@gmail.com>
2024-07-20 09:38:07 +0100

    edit

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * exponential_search - Search in sorted int array
+ * @array: of ints
+ * @size: a number
+ * @value: x
+ * Return: an Integer
+ */
+
+int	exponential_search(int *array, size_t size, int value)
+{
+}
