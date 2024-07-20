030e03d436b18aa6eebd4f93474a2b62ff17a78c
Khalid Benjelloun <khalidmailing@gmail.com>
2024-07-20 22:58:21 +0100

    edit

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -3,7 +3,8 @@
 /**
  * recursive_binary - Search in sorted int array
  * @array: of ints
- * @size: a number
+ * @low: a number
+ * @high: a number
  * @value: x
  * Return: an Integer
  */
@@ -13,6 +14,7 @@ int	recursive_binary(int *array, size_t low, size_t high, int value)
 	size_t	mid;
 	size_t	i;
 
+	mid = low + (high - low) / 2;
 	printf("Searching in array: ");
 	for (i = low; i <= high; i++)
 	{
@@ -22,14 +24,16 @@ int	recursive_binary(int *array, size_t low, size_t high, int value)
 		else
 			printf("\n");
 	}
-	mid = low + (high - low) / 2;
 	if (high >= low)
 	{
 		if (array[mid] == value)
-			return (mid);
-		if (array[mid] > value)
-			return (recursive_binary(array, low, mid - 1, value));
-		return (recursive_binary(array, mid + 1, high, value));
+		{
+			if (mid == 0 || array[mid - 1] != value)
+				return (mid);
+		}
+		if (array[mid] < value)
+			return (recursive_binary(array, mid + 1, high, value));
+		return (recursive_binary(array, low, mid - 1, value));
 	}
 	return (-1);
 }
