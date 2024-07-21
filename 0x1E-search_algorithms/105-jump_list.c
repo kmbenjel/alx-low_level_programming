51f2fb235f4230743cade1c14ae3ea8d28cdf167
Khalid Benjelloun <khalidmailing@gmail.com>
2024-07-21 02:00:30 +0100

    edit

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * jump_list - Search for value
+ * @list: head
+ * @size: size_t
+ * @value: int
+ * Return: int
+ */
+
+listint_t	*jump_list(listint_t *list, size_t size, int value)
+{
+}
